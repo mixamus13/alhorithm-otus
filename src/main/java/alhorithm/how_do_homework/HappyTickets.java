dc2a07b04bb9798d285e5a4b34fa37c88ec6c16c
Max Dokuchaev <mixamus@icloud.com>
2021-11-07 17:11:50 +0300

    Added happy tickets

--- src/main/java/alhorithm/how_do_homework/HappyTickets.java
+++ src/main/java/alhorithm/how_do_homework/HappyTickets.java
@@ -2,10 +2,11 @@ package alhorithm.how_do_homework;
 
 import lombok.extern.slf4j.Slf4j;
 
+// happy ticket = 10 if number  "1234"
 @Slf4j
 public class HappyTickets {
     public static void main(String... args) {
-            lucky();
+        lucky();
     }
 
     private static int lucky() {
@@ -14,7 +15,7 @@ public class HappyTickets {
         for (int a1 = 0; a1 <= 9; a1++) {
             for (int a2 = 0; a2 <= 9; a2++) {
                 for (int a3 = 0; a3 <= 9; a3++) {
-                    for (int a4 = 0; a4 <=9; a4++) {
+                    for (int a4 = 0; a4 <= 9; a4++) {
                         for (int b1 = 0; b1 <= 9; b1++) {
                             for (int b2 = 0; b2 <= 9; b2++) {
                                 for (int b3 = 0; b3 <= 9; b3++) {
@@ -29,6 +30,28 @@ public class HappyTickets {
                     }
                 }
             }
+        }
+        log.info("result: " + count);
+        return count;
+    }
+}
+
+@Slf4j
+class HappyTicketsTwo {
+    public static void main(String... args) {
+        lucky();
+    }
+
+    // happy ticket = 10 if number  "1"
+    private static int lucky() {
+        int count = 0;
+        for (int a1 = 0; a1 <= 9; a1++) {
+            for (int b1 = 0; b1 <= 9; b1++) {
+                if (a1 == b1) {
+                    count++;
+                }
+            }
+
         }
         log.info("result: " + count);
         return count;

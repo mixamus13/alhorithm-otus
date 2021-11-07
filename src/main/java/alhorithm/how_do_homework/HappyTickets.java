8ca140aee3972112718090d5801e3272581aa9bf
Max Dokuchaev <mixamus@icloud.com>
2021-11-07 16:53:22 +0300

    Added happy tickets

---
+++ src/main/java/alhorithm/how_do_homework/HappyTickets.java
@@ -0,0 +1,36 @@
+package alhorithm.how_do_homework;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HappyTickets {
+    public static void main(String... args) {
+            lucky();
+    }
+
+    private static int lucky() {
+
+        int count = 0;
+        for (int a1 = 0; a1 <= 9; a1++) {
+            for (int a2 = 0; a2 <= 9; a2++) {
+                for (int a3 = 0; a3 <= 9; a3++) {
+                    for (int a4 = 0; a4 <=9; a4++) {
+                        for (int b1 = 0; b1 <= 9; b1++) {
+                            for (int b2 = 0; b2 <= 9; b2++) {
+                                for (int b3 = 0; b3 <= 9; b3++) {
+                                    for (int b4 = 0; b4 <= 9; b4++) {
+                                        if (a1 + a2 + a3 + a4 == b1 + b2 + b3 + b4) {
+                                            count++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        log.info("result: " + count);
+        return count;
+    }
+}
